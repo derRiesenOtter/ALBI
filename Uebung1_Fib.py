18aaf9e862e4c9200ce7e8cb429952d2fafa5e73
derRiesenOtter <robin.ender@icloud.com>
2023-04-20 11:13:14 +0200

    unittest etc

--- Uebung1_Fib.py
+++ Uebung1_Fib.py
@@ -1,18 +1,41 @@
+import unittest
 
-def Fib(n):
+def fibonacci(x):
+    try:
+        n = int(x)
+    except:
+        raise ValueError("Wrong Input. Please enter an integer as argument")
+    print("The first" , n, "Fibonacci numbers are:")
+    for i in range (1, n):
+        print(fib(i), ", ", sep = "", end="")
+    print(fib(n))
+    return fib(n)
+
+def fib(n):
     if n == 1:
         return 1
     if n == 0:
         return 0
-    return Fib(n-1) + Fib(n-2)
+    return fib(n-1) + fib(n-2)
 
 print("Please enter how many Fibonacci numbers you want to list:")
+x = input()
+fibonacci(x)
 
-n = int(input())
+# Test
 
-print("The first" , n, "Fibonacci numbers are:")
+class TestFib(unittest.TestCase):
+    def test_InputHandling(self):
+        self.assertRaises(ValueError, fibonacci, "a"), "ValueError should occur"
+    def test_Result(self):
+        assert fib(6) == 8, "Result should be 8"
+    
+if __name__ == "__main__":
+    unittest.main()
+    
 
-for i in range (1, n):
-    print(Fib(i), ", ", sep = "", end="")
-print(Fib(n))
+print("Please enter how many Fibonacci numbers you want to list:")
+x = input()
+fibonacci(x)
+
 
